5df294ac727a31a0195a48ddbf963dca01e68ed0
ikari-shirei <algrsyu@protonmail.com>
2021-09-12 22:42:38 +0300

    Add 3.16phonebook database

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const Person = require('./models/person')
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
-app.use(express.static('build'))
 
 app.set('Content-Security-Policy', "default-src 'none'")
 
@@ -43,10 +43,12 @@ Person.countDocuments({}, function (err, count) {
 })
   .then((value) => {
     personsLength = value
+
     return value
   })
   .catch((err) => {
     console.log(err)
+
     return 'ERROR'
   })
 
@@ -64,58 +66,44 @@ app.get('/info', (request, response) => {
   )
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find((p) => p.id === id)
-
-  person ? response.json(person) : response.status(404).end()
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      person ? response.json(person) : response.status(404).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((p) => p.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (body.name === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
+  Person.find({}).then((result) => {
+    const isNameExist = result.find((person) => body.name === person.name)
 
-  Person.find({})
-    .then((result) => {
-      return result
-    })
-    .then((result) => {
-      const isNameExist = result.find((person) => body.name === person.name)
+    if (!body.name || !body.number) {
+      next({ name: 'ContentMissing' })
+    } else if (isNameExist) {
+      next({ name: 'AlreadyAdded' })
+    } else {
+      const newPerson = new Person({
+        name: body.name,
+        number: body.number,
+        date: new Date(),
+      })
 
-      if (!body.name) {
-        return response.status(400).json({
-          error: 'there must be a name',
-        })
-      } else if (!body.number) {
-        return response.status(400).json({
-          error: 'there must be number',
-        })
-      } else if (isNameExist) {
-        return response.status(400).json({
-          error: 'name must be unique',
-        })
-      } else {
-        const newPerson = new Person({
-          name: body.name,
-          number: body.number,
-          date: new Date(),
-        })
-
-        newPerson.save().then((savedNote) => {
-          response.json(savedNote)
-        })
-      }
-    })
+      newPerson.save().then((savedNote) => {
+        response.json(savedNote)
+      })
+    }
+  })
 })
 
 const PORT = process.env.PORT || 3001
@@ -123,3 +111,25 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ContentMissing') {
+    return response.status(400).json({ error: 'content missing' })
+  } else if (error.name === 'AlreadyAdded') {
+    return response.status(400).json({ error: 'name is already in the list' })
+  }
+
+  next(error)
+}
+
+//last loaded
+app.use(errorHandler)
