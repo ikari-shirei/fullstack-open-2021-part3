b2d6eefad90d45bb2094df0b232cf6bc7200075b
ikari-shirei <algrsyu@protonmail.com>
2021-09-14 03:47:55 +0300

    Add 3.21 phonebook database

--- index.js
+++ index.js
@@ -67,7 +67,10 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+  })
     .then((updatedNote) => {
       response.json(updatedNote)
     })
